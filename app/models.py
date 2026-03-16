51f430f07654920acea9b3f598bbfe51b1e39724
Vitalii Ivanchenko <vanno1234567890@gmail.com>
2026-03-16 15:14:07 +0200

    Initialize database structure and Alembic migrations

---
+++ app/models.py
@@ -0,0 +1,46 @@
+import enum
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, DateTime, Enum, Text
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+from app.database import Base
+
+class UserGroupEnum(str, enum.Enum):
+    USER = "USER"
+    ADMIN = "ADMIN"
+    MODERATOR = "MODERATOR"
+
+class GenderEnum(str, enum.Enum):
+    MAN = "MAN"
+    WOMAN = "WOMAN"
+
+class UserGroup(Base):
+    __tablename__ = "user_groups"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(Enum(UserGroupEnum), nullable=False, unique=True)
+
+    users = relationship("User", back_populates="group")
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    is_active = Column(Boolean, default=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
+
+    group_id = Column(Integer, ForeignKey("user_groups.id"))
+
+    group = relationship("UserGroup", back_populates="users")
+    profile = relationship("UserProfile", back_populates="user", uselist=False)
+
+class UserProfile(Base):
+    __tablename__ = "user_profiles"
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"), unique=True)
+    first_name = Column(String, nullable=True)
+    last_name = Column(String, nullable=True)
+    gender = Column(Enum(GenderEnum), nullable=True)
+    info = Column(Text, nullable=True)
+
+    user = relationship("User", back_populates="profile")
