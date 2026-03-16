9680f953a9b86888e2d2eae3bcf19dea0684bf2f
Vitalii Ivanchenko <vanno1234567890@gmail.com>
2026-03-16 18:04:35 +0200

    Set up JWT infrastructure: add environment variables support and token generation logic

--- app/schemas/users.py
+++ app/schemas/users.py
@@ -34,3 +34,11 @@ class UserResponse(BaseModel):
     is_active: bool
 
     model_config = ConfigDict(from_attributes=True)
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    email: Optional[str] = None
--- app/security.py
+++ app/security.py
@@ -1,13 +1,36 @@
+import os
 import bcrypt
+from jose import jwt
+from datetime import datetime, timedelta, timezone
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 def get_password_hash(password: str) -> str:
     pwd_bytes = password.encode("utf-8")
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(pwd_bytes, salt)
+
     return hashed_password.decode("utf-8")
+
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     password_bytes = plain_password.encode("utf-8")
     hashed_bytes = hashed_password.encode("utf-8")
 
     return bcrypt.checkpw(password_bytes, hashed_bytes)
+
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expired_time = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expired_time})
+
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
